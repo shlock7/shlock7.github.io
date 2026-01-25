801ab797f3cfe73182b3788c72c7225e66b6318f
shlock7 <1397277295@qq.com>
2026-01-25 10:58:52 +0800

    Version-init

--- site.config.ts
+++ site.config.ts
@@ -1,11 +1,11 @@
 import siteConfig from "./src/utils/config";
 
 const config = siteConfig({
-	title: "ThoughtLite",
+	title: "码到成功",
 	prologue: "If you need a website\nthat loads fast and has great SEO, then Astro is for you.",
 	author: {
-		name: "Your Name",
-		email: "hi@your.mail",
+		name: "Hank Sun",
+		email: "shk1717@163.com",
 		link: "https://your.website"
 	},
 	description: "A modern Astro theme focused on content creation.",
@@ -14,8 +14,8 @@ const config = siteConfig({
 		year: "2025"
 	},
 	i18n: {
-		locales: ["en", "zh-cn", "ja"],
-		defaultLocale: "en"
+		locales: ["zh-cn"],
+		defaultLocale: "zh-cn"
 	},
 	pagination: {
 		note: 15,
